5005b7b6a9b24c0d67d16582f6ece96be787437c
Lina Trojaike <linatrojaike@gmail.com>
2024-11-19 10:25:38 -0300

    Primeiro commit - Landing Page BK

---
+++ js/dist/scripts.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var slide_combos = new Swiper('.slide-combos', {
+  slidesPerView: 3,
+  spaceBetween: 32,
+  speed: 800,
+  paginations: {
+    el: '',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '',
+    prevEl: ''
+  }
+});
+//# sourceMappingURL=scripts.dev.js.map
---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var slide_combos = new Swiper('.slide-combos', {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    speed: 800,
+    paginations: {
+        el: '',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '',
+        prevEl:''
+    }
+})
